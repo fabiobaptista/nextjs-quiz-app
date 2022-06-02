f9882e1bfb54586cdb6ac3d633ddb89425a5af76
fabiobaptista <fhbaptista@gmail.com>
2022-06-02 16:23:27 -0300

    test: ensure save answer to question

---
+++ src/data/usecases/save-answer/SaveAnswerUseCase.test.ts
@@ -0,0 +1,62 @@
+import FetchHttpClient from '../../../infra/http/FetchHttpClient'
+import 'isomorphic-fetch'
+import { InputCreateRoundDto } from '../create-round/CreateRoundDto'
+import { InputSaveAnswerDto, OutputSaveAnswerDto } from './SaveAnswerDto'
+import SaveAnswerUseCase from './SaveAnswerUseCase'
+
+const playerDataMock = {
+  player_name: 'fabio',
+  category_id: 3
+}
+
+const urlApi  = `https://test-quiz-app-backend.herokuapp.com/rounds`
+const httpClient = new FetchHttpClient()
+
+describe('Test Create Round Use Case', () => {
+  test('should create a round', async () => {
+
+    const reqCreate = await httpClient.request({
+      url: urlApi,
+      method: 'post',
+      headers: {
+          'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+          round: {
+              ...playerDataMock
+          }
+      }),
+  })
+
+  const round:any = reqCreate.body
+
+  expect(round.round.id).toBeGreaterThan(0)
+  expect(round.round.answers.length).toEqual(0)
+  
+    const input: InputSaveAnswerDto = {
+      roundId: round.round.id,
+      questionId: round.round.questions[0].id,
+      optionId: round.round.questions[0].options[0].id
+    }
+
+    const usecase = new SaveAnswerUseCase(urlApi, httpClient)
+
+    const output: OutputSaveAnswerDto = await usecase.execute(input)
+
+    console.log(output)
+    expect(output.answer?.id).toBeGreaterThan(0)
+    expect(output.answer?.option_id).toBe(round.round.questions[0].options[0].id)
+  })
+
+  // test('should not create a round', async () => {
+
+  //   const input: InputCreateRoundDto = inputRoundMock
+  //   const usecase = new CreateRoundUseCase(urlApi, httpClient)
+
+  //   input.categoryId = 0
+
+  //   const output: OutputCreateRoundDto = await usecase.execute(input)
+
+  //   expect(output).toStrictEqual({})
+  // })
+})
