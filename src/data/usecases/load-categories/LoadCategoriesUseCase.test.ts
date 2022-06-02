41fe121ee689b5d78adf95e25f24c20cf2e14144
fabiobaptista <fhbaptista@gmail.com>
2022-06-02 09:58:16 -0300

    refactor: remove HttpClientFactory in integration test

--- src/data/usecases/load-categories/LoadCategoriesUseCase.test.ts
+++ src/data/usecases/load-categories/LoadCategoriesUseCase.test.ts
@@ -1,14 +1,13 @@
-import HttpClientFactory from '../../../factory/HttpClientFactory'
+
 import { InputLoadCategoriesDto, OutputLoadCategoriesDto } from './LoadCategoriesDto'
 import LoadCategoriesUseCase from './LoadCategoriesUseCase'
+import FetchHttpClient from '../../../infra/http/FetchHttpClient'
 import 'isomorphic-fetch'
 
 const urlApi  = `https://test-quiz-app-backend.herokuapp.com/categories`
-
+const httpClient = new FetchHttpClient()
 describe('Test Integration Load Categories Use Case', () => {
   test('should returns categories empty list', async () => {
-    
-    const httpClient = HttpClientFactory.Fetch()
     const input: InputLoadCategoriesDto = { }
     const usecase = new LoadCategoriesUseCase(`${urlApi}aaa`, httpClient)
 
@@ -18,8 +17,6 @@ describe('Test Integration Load Categories Use Case', () => {
   })
 
   test('should returns categories empty list', async () => {
-    
-    const httpClient = HttpClientFactory.Fetch()
     const input: InputLoadCategoriesDto = { }
     const usecase = new LoadCategoriesUseCase(urlApi, httpClient)
 
